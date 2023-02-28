a549e7f8ea7ca1e8d053eb7d98592423c14995a2
sullivannicole <nicasull@gmail.com>
2023-02-28 04:20:43 +0000

    updated functions now have try-catch block, so run for a few more entries

--- dps_data.R
+++ dps_data.R
@@ -7,7 +7,7 @@ library(stringr)
 library(tidyverse)
 library(glue)
 
-path_prefix <- '/Users/nicolesullivan/Documents/Academic/2021-2023/MS_in_DS/Coursework/2023/Spring/CSCI_8980/data/dps_data'
+path_prefix <- '/Users/nicolesullivan/Documents/Academic/2021-2023/MS_in_DS/Coursework/2023/Spring/CSCI_8980/data'
 source(glue('{path_prefix}/dps_functions.R'))
 
 # ---------------
@@ -21,5 +21,5 @@ pg_nums <- 40400:40480
 incident_df <- map_dfr(pg_nums, run_incident_pull)
 vehicles_df <- map_dfr(pg_nums, run_vehicle_pull)
 
-write_csv(incident_df, glue('{path_prefix}/incidents_{current_dttm}.csv'))
-write_csv(vehicles_df, glue('{path_prefix}/vehicles_{current_dttm}.csv'))
+write_csv(incident_df, glue('{path_prefix}/dps_data/incidents_{current_dttm}.csv'))
+write_csv(vehicles_df, glue('{path_prefix}/dps_data/vehicles_{current_dttm}.csv'))
