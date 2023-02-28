37d10e2d9f8c6cdd66ada79e45b90510d2311ebc
sullivannicole <nicasull@gmail.com>
2023-02-28 03:19:17 +0000

    add script to generate dataframes from scraping DPS site

---
+++ dps_data.R
@@ -0,0 +1,25 @@
+# --------------
+# Libraries
+# --------------
+library(rvest)
+library(xml2)
+library(stringr)
+library(tidyverse)
+library(glue)
+
+path_prefix <- '/Users/nicolesullivan/Documents/Academic/2021-2023/MS_in_DS/Coursework/2023/Spring/CSCI_8980/data/dps_data'
+source(glue('{path_prefix}/dps_functions.R'))
+
+# ---------------
+# Pull data
+# ---------------
+
+current_dttm <- str_replace_all(Sys.time(), ':|CST', '_')
+
+pg_nums <- 40400:40480
+
+incident_df <- map_dfr(pg_nums, run_incident_pull)
+vehicles_df <- map_dfr(pg_nums, run_vehicle_pull)
+
+write_csv(incident_df, glue('{path_prefix}/incidents_{current_dttm}.csv'))
+write_csv(vehicles_df, glue('{path_prefix}/vehicles_{current_dttm}.csv'))
